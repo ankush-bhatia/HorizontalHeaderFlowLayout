5c0cf737149708306dda91fd4f9a896bb7cec8d0
Ankush Bhatia <ankush.bhatia@mutualmobile.com>
2019-08-11 20:44:48 +0530

    Podspec Added.

---
+++ HorizontalHeaderFlowLayout/HorizontalHeaderFlowLayout.h
@@ -0,0 +1,19 @@
+//
+//  HorizontalHeaderFlowLayout.h
+//  HorizontalHeaderFlowLayout
+//
+//  Created by Ankush Bhatia on 11/08/19.
+//  Copyright © 2019 Ankush Bhatia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HorizontalHeaderFlowLayout.
+FOUNDATION_EXPORT double HorizontalHeaderFlowLayoutVersionNumber;
+
+//! Project version string for HorizontalHeaderFlowLayout.
+FOUNDATION_EXPORT const unsigned char HorizontalHeaderFlowLayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HorizontalHeaderFlowLayout/PublicHeader.h>
+
+
